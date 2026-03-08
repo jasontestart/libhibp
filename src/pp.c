94671eaabe2044bf861fa7f0c80a5e1ca3bce28d
Jason Testart <76924770+jasontestart@users.noreply.github.com>
2026-03-08 15:19:03 -0400

    tabs v spaces fail

--- src/pp.c
+++ src/pp.c
@@ -29,7 +29,7 @@ static void to_hex(const unsigned char *digest, char *output) {
 
 int generate_sha1_digest(char *password, size_t size, char *hex_digest) {
     unsigned char message_digest[SHA1_DIGEST_LENGTH_BYTES];
-
+	
     if (!SHA1((unsigned char*)password, size, message_digest)) {
         return -1;
     }
@@ -73,7 +73,7 @@ static size_t write_callback(void *contents, size_t size, size_t nmemb, void *us
 
 /* Look for the hash in the pwned password database */
 int find_matching_entry(char hash[SHA1_HEX_DIGEST_LENGTH + 1], 
-		PwnedResultStruct *pwned_result, char *proxy_server_url, char *api_url) {
+    PwnedResultStruct *pwned_result, char *proxy_server_url, char *api_url) {
 
     /* Google says URLs should be less that 2000 characters. */
     char url[1792];
@@ -83,8 +83,8 @@ int find_matching_entry(char hash[SHA1_HEX_DIGEST_LENGTH + 1],
     const char *hash_suffix = hash + API_HASH_PREFIX_LENGTH;
 
     if (!pwned_result) {
-	hibp_internal_log("NULL pointer pwned_result passed to function find_matching_entry.");
-        return -1;
+		hibp_internal_log("NULL pointer pwned_result passed to function find_matching_entry.");
+		return -1;
     }
 
     /* Construct the url we are passing to curl */
@@ -103,24 +103,25 @@ int find_matching_entry(char hash[SHA1_HEX_DIGEST_LENGTH + 1],
 
     curl = curl_easy_init();
     if(curl) {
-	/* set-up the headers */
-	struct curl_slist *headers = NULL;
-	headers = curl_slist_append(headers, "Accept: text/plain");
-        headers = curl_slist_append(headers, "Add-Padding: true");
-	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		/* set-up the headers */
+		struct curl_slist *headers = NULL;
+		headers = curl_slist_append(headers, "Accept: text/plain");
+		headers = curl_slist_append(headers, "Add-Padding: true");
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-	curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
-	curl_easy_setopt(curl, CURLOPT_USERAGENT, "MyHIBP-Security-Module/1.0");
-	/* 2 seconds for the connection (DNS + TCP Handshake) */
-        curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 2L);
-	/* 5 seconds total for the entire transaction */
-	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+		curl_easy_setopt(curl, CURLOPT_USERAGENT, "MyHIBP-Security-Module/1.0");
+		
+		/* TIMEOUT: 2 seconds for the connection (DNS + TCP Handshake) */
+		curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 2L);
+		/* TIMEOUT: 5 seconds total for the entire transaction */
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
 
-	if (proxy_server_url != NULL)
-	    curl_easy_setopt(curl, CURLOPT_PROXY, proxy_server_url);
+		if (proxy_server_url != NULL)
+			curl_easy_setopt(curl, CURLOPT_PROXY, proxy_server_url);
 
         /* Perform the request, result gets the return code */
         result = curl_easy_perform(curl);
@@ -128,50 +129,51 @@ int find_matching_entry(char hash[SHA1_HEX_DIGEST_LENGTH + 1],
         /* Check for errors */
         if(result != CURLE_OK) {
             hibp_internal_log("curl_easy_perform() failed");
-	    hibp_internal_log(curl_easy_strerror(result));
-	} else {
-	    pwned_result->occurences = 0; /* initialize */
-	    char *match;
-	    memset(pwned_result->hash, 0, sizeof(pwned_result->hash));
-	    match = strstr(chunk.memory, hash_suffix);
-	    if (match) {
+            hibp_internal_log(curl_easy_strerror(result));
+        } else {
+			pwned_result->occurences = 0; /* initialize */
+			char *match;
+			memset(pwned_result->hash, 0, sizeof(pwned_result->hash));
+			match = strstr(chunk.memory, hash_suffix);
+			if (match) {
 
-		/* This block of code is really just a sanity check for the assertion. */
-		memcpy(pwned_result->hash, hash, API_HASH_PREFIX_LENGTH);
-	        int j = 0;
-		int i;
-	        for (i = API_HASH_PREFIX_LENGTH; i < SHA1_HEX_DIGEST_LENGTH; i++) {
-	            pwned_result->hash[i] = match[j];
-		    j++;
-	        }
+				/* This block of code is really just a sanity check for the assertion. */
+				memcpy(pwned_result->hash, hash, API_HASH_PREFIX_LENGTH);
+				int j = 0;
+				int i;
+				for (i = API_HASH_PREFIX_LENGTH; i < SHA1_HEX_DIGEST_LENGTH; i++) {
+					pwned_result->hash[i] = match[j];
+					j++;
+				}
 
-		/* Validate data in the results struct */
+				/* Validate data in the results struct */
                 assert(strcmp(hash, pwned_result->hash) == 0);
 
-	        /* Now get the occurences - can't be more than 10 digits, right? */
-	        char occ_str[10];
-	        memset(occ_str, 0, sizeof(occ_str));
-	        j++;
-	        i = 0;
-	        while (match[j] != '\r' && match[j] != '\0' && match[j] != '\n') {
-	            occ_str[i] = match[j];
-	    	    i++;
-		    j++;
-		    if (i > 10)
-		        return -1;
-	        }
+				/* Now get the occurences - can't be more than 10 digits, right? */
+				char occ_str[10];
+				memset(occ_str, 0, sizeof(occ_str));
+				j++;
+				i = 0;
+				while (match[j] != '\r' && match[j] != '\0' && match[j] != '\n') {
+					occ_str[i] = match[j];
+					i++;
+					j++;
+					if (i > 10)
+						return -1;
+				}
 
-	        pwned_result->occurences = atoll(occ_str);
-		/* if we get a zero, then this probably a false positive from padding. */
-		if (pwned_result->occurences == 0) {
-		    memset(pwned_result->hash, 0, sizeof(pwned_result->hash));
+				pwned_result->occurences = atoll(occ_str);
+				
+				/* if we get a zero, then this probably a false positive from padding. */
+				if (pwned_result->occurences == 0) {
+					memset(pwned_result->hash, 0, sizeof(pwned_result->hash));
+				}
+			}
 		}
-	    }
-	}
 
-        /* always cleanup */
-	curl_slist_free_all(headers);
-        curl_easy_cleanup(curl);
+		/* always cleanup */
+		curl_slist_free_all(headers);
+		curl_easy_cleanup(curl);
     }
     free(chunk.memory);
     return (int)result;
